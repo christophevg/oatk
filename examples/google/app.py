8eaca525921491389ab1106fbf91c0415385c71f
Christophe VG <contact@christophe.vg>
2023-04-15 14:44:24 +0200

    serving oatk.js from package

--- examples/google/app.py
+++ examples/google/app.py
@@ -6,9 +6,10 @@ load_dotenv(find_dotenv(".env.local"))
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from flask_restful import Resource, Api
 
+import oatk.js
 from oatk import OAuthToolkit
 
 server = Flask(__name__)
@@ -18,19 +19,24 @@ server = Flask(__name__)
 def home():
   return render_template("home.html", **os.environ)
 
+# route service oatk.js from the oatk package
+@server.route("/oatk.js", methods=["GET"])
+def oatk_script():
+  return Response(oatk.js.as_src(), mimetype="application/javascript")
+
 # API set up
 api = Api(server)
 
 # setup oatk
-oatk = OAuthToolkit()
-oatk.using_provider(os.environ["OAUTH_PROVIDER"]);
-oatk.with_client_id(os.environ["OAUTH_CLIENT_ID"])
+auth = OAuthToolkit()
+auth.using_provider(os.environ["OAUTH_PROVIDER"]);
+auth.with_client_id(os.environ["OAUTH_CLIENT_ID"])
 
 def validate_name(name):
   return name == "Christophe VG"
 
 class HelloWorld(Resource):
-  @oatk.authenticated_with_claims(name=validate_name)
+  @auth.authenticated_with_claims(name=validate_name)
   def get(self):
     return {"hello": "world"}
 
